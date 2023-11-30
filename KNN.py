560f056af72db29a4251a61768442458318d569e
Juan Selame <jselame@emory.edu>
2023-11-30 16:12:39 -0500

    Optimial N + AUROC/AUPRC Evaluation for KNN

--- KNN.py
+++ KNN.py
@@ -3,6 +3,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import roc_auc_score, average_precision_score
+from sklearn.metrics import roc_auc_score, average_precision_score
 
 # Load the dataset
 df = pd.read_csv('LongCovidData.csv')
@@ -34,6 +36,59 @@ knn.fit(X_train, y_train)
 # Make predictions
 predictions = knn.predict(X_test)
 
-# Evaluate the model
+# Basic evaluation with classification report and confusion matrix
 print(classification_report(y_test, predictions))
 print(confusion_matrix(y_test, predictions))
+
+# Additional evaluation for imbalanced dataset
+roc_auc = roc_auc_score(y_test, knn.predict_proba(X_test)[:, 1])
+
+# Calculate AUPRC
+precision_recall_auc = average_precision_score(y_test, knn.predict_proba(X_test)[:, 1])
+
+print("AUROC: ", roc_auc)
+print("AUPRC: ", precision_recall_auc)
+
+
+############### Code to find most acurrate K ###############
+
+# best_auroc = 0
+# best_auprc = 0
+# best_k_auroc = 0
+# best_k_auprc = 0
+
+# for k in range(1, 26):  # Test a range of k values
+#     knn = KNeighborsClassifier(n_neighbors=k)
+#     knn.fit(X_train, y_train)
+    
+#     # Getting the probabilities for positive class
+#     y_proba = knn.predict_proba(X_test)[:, 1]
+
+#     # Calculating AUROC and AUPRC
+#     auroc = roc_auc_score(y_test, y_proba)
+#     auprc = average_precision_score(y_test, y_proba)
+
+#     if auroc > best_auroc:
+#         best_auroc = auroc
+#         best_k_auroc = k
+
+#     if auprc > best_auprc:
+#         best_auprc = auprc
+#         best_k_auprc = k
+
+# print(f"Best K Value for AUROC: {best_k_auroc} with AUROC: {best_auroc}")
+# print(f"Best K Value for AUPRC: {best_k_auprc} with AUPRC: {best_auprc}")
+
+# -----------------------------------------------------------------------
+# Best K Value for AUROC: 25 with AUROC: 0.6261863206648385
+# Best K Value for AUPRC: 25 with AUPRC: 0.3166988682880273
+
+# For k = 5 AUROC:  0.5753610785795892
+# for k = 5 AUPRC:  0.2656687275344795
+
+# The increase in time complexity probably isnt a good tradeoff for the increase in AUROC and AUPRC accuracy
+
+
+
+
+
